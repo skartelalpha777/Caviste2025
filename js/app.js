79dd46f3d05a63fd7d2a9685b27595fc68959423
Mamadou Alpha Diallo <3010madiallo@student.epfc.eu>
2025-10-14 14:57:50 +0200

    fichier JS completé mais avec erreur

--- js/app.js
+++ js/app.js
@@ -4,8 +4,26 @@ const API_URL = 'https://cruth.phpnet.org/epfc/caviste/public/index.php/api';
 fetch('https://cruth.phpnet.org/epfc/caviste/public/index.php/api/wines')
     .then(response => response.json())
     .then(json => {
+
+        const ULWineList = document.querySelector('wineList');
+        ULWineList.innerHTML ='';
         json.forEach(wine => {
             console.log(wine.name);
+            let LI = document.createElement('li');
+            LI.innerHTML = wine.name;
+            LI.classList.add('list-groupep-item');
+            LI.dataset.wineId = wine.id;
+            LI.addEventListener('click', function (e) {
+
+                let wineId = LI.dataset.wineId;
+                let results = data.filter(wine == wine.id);
+                let wine = results.length > 0 ? results[0] : undefined;
+                console.log(wine);
+
+            });
+
+            ULWineList.appendChild(Li);
+
         });
 
     });
